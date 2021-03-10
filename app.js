18bdc320bfebabf95c932a2ca23c9a05892ea631
Vitor Quadros <viitorquadros@hotmail.com>
2021-03-10 05:04:09 -0300

    feat (Processor): data processor method

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const Reader = require("./classes/Reader");
+const Processor = require("./classes/Processor");
 
 const reader = new Reader();
 
 async function main() {
     const data = await reader.read("./test.csv");
-    console.log(data);
+    const processedData = Processor.process(data);
 }
 
 main();
