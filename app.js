b56f78876f8d2b34b730e6d6b4ef1ee28ad8f923
Vitor Quadros <viitorquadros@hotmail.com>
2021-03-10 22:13:53 -0300

    feat (Table): row/column count

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const Reader = require("./classes/Reader");
 const Processor = require("./classes/Processor");
+const Table = require("./classes/Table");
 
 const reader = new Reader();
+
 
 async function main() {
     const data = await reader.read("./test.csv");
     const processedData = Processor.process(data);
+    const table = new Table(processedData);
+    console.log(table.rows);
 }
 
 main();
